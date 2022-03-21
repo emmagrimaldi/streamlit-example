f6ce4732e103617c58162cfd4dde8203f99d7c41
Emma Grimaldi <emmagrimaldi@gmail.com>
2022-03-21 14:26:26 -0700

    added 'from'

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ def run_query(query):
         return cur.fetchall()
 
 
-rows = run_query("SELECT * SCORES.CUSTOM_TURNOUT_SCORES_METADATA;")
+rows = run_query("SELECT * FROM SCORES.CUSTOM_TURNOUT_SCORES_METADATA;")
 
 # Print results.
 for row in rows:
